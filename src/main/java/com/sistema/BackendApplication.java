c7ef15ace4d7cb8285780de72edd4d758e289c71
diego gimenez <diegote58@gmail.com>
2017-11-23 18:58:22 -0300

    Proyecto Base.

---
+++ src/main/java/com/sistema/BackendApplication.java
@@ -0,0 +1,19 @@
+package com.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@SpringBootApplication
+@EnableScheduling
+public class BackendApplication {
+	  	
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+}
